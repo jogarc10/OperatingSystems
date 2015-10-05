3788cac8a449bd7b4de01bbc984537bb7a324502
Jorge Garcia <silversoldier9@hotmail.com>
2015-10-05 20:00:33 +0200

    Example 4 added

---
+++ example4.c
@@ -0,0 +1,24 @@
+// One dimensional array. Example from page 11 of intro to c slides.
+
+#include <stdio.h>
+int main(void) {
+	int number[12]; // 12 cells, one per students
+	int index, sum = 0;
+
+	/* Always initialize array before use  */
+
+	for (index = 0; index < 12; index++) {
+		printf("Array before assignation: %d \n", number[index]);
+		number[index] = index;	
+	}
+
+	/* If I try to access the array with index after the for, will cause 
+           an error due index is 12... (sums 1 before exitsting loop) */
+
+	for (index = 0; index < 12; index++) {
+		sum += number[index]; // sum array elements
+	}
+	printf("------------\n");
+	printf("Total sum of the array: %d\n", sum);
+	return 1;
+}
